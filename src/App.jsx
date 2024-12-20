737986f910dd03c7729b55011676a18bb4722f06
anandamritesh <anand.amri@northeastern.edu>
2024-12-19 17:28:17 -0800

    adding props

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,8 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <Hero title='Test Title' subtitle='This is the subtitle' />
+      <Hero />
 
-      {/* <!-- Developers and Employers --> */}
       <section className='py-4'>
         <div className='container-xl lg:container m-auto'>
           <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
