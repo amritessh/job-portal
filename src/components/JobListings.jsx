78bfca1e6b19d5e749b6722be82194e802eebd4f
anandamritesh <anand.amri@northeastern.edu>
2024-12-20 13:25:08 -0800

    updating dependencies

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,53 +1,52 @@
-// import React from 'react'
-import jobs from '../jobs.json';
+import { useState } from 'react';
+import { FaMapMarker } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+// import job from '../jobs.json';
 
-const JobListings = () => {
+const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + '...';
+  }
+
   return (
-    <>
-      <section className='py-4'>
-        <div className='container-xl lg:container m-auto'>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
-            <div className='bg-gray-100 p-6 rounded-lg shadow-md'>
-              <h2 className='text-2xl font-bold'>For Developers</h2>
-              <p className='mt-2 mb-4'>
-                Browse our React jobs and start your career today
-              </p>
-              <a
-                href='/jobs.html'
-                className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700'
-              >
-                Browse Jobs
-              </a>
-            </div>
-            <div className='bg-indigo-100 p-6 rounded-lg shadow-md'>
-              <h2 className='text-2xl font-bold'>For Employers</h2>
-              <p className='mt-2 mb-4'>
-                List your job to find the perfect developer for the role
-              </p>
-              <a
-                href='/add-job.html'
-                className='inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600'
-              >
-                Add Job
-              </a>
-            </div>
-          </div>
+    <div className='bg-white rounded-xl shadow-md relative'>
+      <div className='p-4'>
+        <div className='mb-6'>
+          <div className='text-gray-600 my-2'>{job.type}</div>
+          <h3 className='text-xl font-bold'>{job.title}</h3>
         </div>
-      </section>
 
-      {/* <!-- Browse Jobs --> */}
-      <section className='bg-blue-50 px-4 py-10'>
-        <div className='container-xl lg:container m-auto'>
-          <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-            Browse Jobs
-          </h2>
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {jobs.map}
+        <div className='mb-5'>{description}</div>
+
+        <button
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+          className='text-indigo-500 mb-5 hover:text-indigo-600'
+        >
+          {showFullDescription ? 'Less' : 'More'}
+        </button>
+
+        <h3 className='text-indigo-500 mb-2'>{job.salary} / Year</h3>
+
+        <div className='border border-gray-100 mb-5'></div>
+
+        <div className='flex flex-col lg:flex-row justify-between mb-4'>
+          <div className='text-orange-700 mb-3'>
+            <FaMapMarker className='inline text-lg mb-1 mr-1' />
+            {job.location}
           </div>
+          <Link
+            to={`/jobs/${job.id}`}
+            className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'
+          >
+            Read More
+          </Link>
         </div>
-      </section>
-    </>
+      </div>
+    </div>
   );
 };
-
-export default JobListings;
+export default JobListing;
