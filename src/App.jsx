9211b3fe74dc2c0424b9a785c623a2be86643d7f
anandamritesh <anand.amri@northeastern.edu>
2024-12-29 19:04:34 -0500

    adding layouts

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,18 @@ import {
   RouterProvider
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MainLayout from './Layouts/MainLayout';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/about' element={<HomePage />} />)
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route path='/about' element={<HomePage />} />
+    </Route>
+  )
 );
 
 const App = () => {
   return <RouterProvider router={router} />;
-  // <>
-  //   <Navbar />
-  //   <Hero />
-  //   <HomeCards />
-  //   <JobListings />
-  //   <ViewAllJobs />
-  // </>
 };
 
 export default App;
