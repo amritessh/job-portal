1479e1bbbfcbfa5e5dacddee96a5b52a4b06ae07
anandamritesh <anand.amri@northeastern.edu>
2025-01-01 15:37:26 -0500

    job page using hooks

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,7 +1,27 @@
 // import React from 'react';
 
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 const JobPage = () => {
-  return <>Job ID Page</>;
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  });
+  return loading ? '' : <h1>{job.title}</h1>;
 };
 
 export default JobPage;
