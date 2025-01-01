f7d8dbb2e1a4685e217d0638eb048c8c33619738
anandamritesh <anand.amri@northeastern.edu>
2024-12-31 19:28:57 -0500

    adding spinner in job listings

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -30,11 +30,15 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
 
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          {jobs.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
-        </div>
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
