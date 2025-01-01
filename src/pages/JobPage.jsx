e62a2f8a161535e399cf4cb928532a68c7c66656
anandamritesh <anand.amri@northeastern.edu>
2025-01-01 15:42:24 -0500

    react router data loaders

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,27 +1,36 @@
 // import React from 'react';
 
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const JobPage = () => {
   const { id } = useParams();
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchJob = async () => {
-      try {
-        const res = await fetch(`/api/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-      } catch (error) {
-        console.log('Error fetching data', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-  });
+  //   const [job, setJob] = useState(null);
+  //   const [loading, setLoading] = useState(true);
+
+  //   useEffect(() => {
+  //     const fetchJob = async () => {
+  //       try {
+  //         const res = await fetch(`/api/jobs/${id}`);
+  //         const data = await res.json();
+  //         setJob(data);
+  //       } catch (error) {
+  //         console.log('Error fetching data', error);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     };
+  //   });
   return loading ? '' : <h1>{job.title}</h1>;
 };
 
-export default JobPage;
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jpbs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
+
+// export default JobPage;
